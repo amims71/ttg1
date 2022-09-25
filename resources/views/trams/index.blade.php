e4df5f694725bf3f37e728c031fb932baad73b91
monirul89 <monirul89@gmail.com>
2022-09-25 17:49:42 +0600

    update

--- resources/views/trams/index.blade.php
+++ resources/views/trams/index.blade.php
@@ -5,16 +5,16 @@
 @section('section-content')
 
 	<!-- Start Page Title Area -->
-	<div class="page-title-area">
+	<div class="page-title-area item-bg1">
 		<div class="d-table">
 			<div class="d-table-cell">
 				<div class="container">
 					<div class="page-title-content">
-						<h2>Contact</h2>
+						<h2>Terms & Conditions</h2>
 						<ul>
 							<li><a href="index.html">Home</a>
 							</li>
-							<li>Contact</li>
+							<li>Terms & Conditions</li>
 						</ul>
 					</div>
 				</div>
@@ -22,102 +22,56 @@
 		</div>
 	</div>
 	<!-- End Page Title Area -->
-
-	<!-- Start Contact Box Area -->
-	<section class="contact-info-wrapper">
+	
+	<!-- Start Inner Privacy Policy Area -->
+	<section class="privacy-policy ptb-100">
 		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="section-title">
-						<h6>Contact Information</h6>
-						<h2>Find Us</h2>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="contact-info-content">
-						<h5>Bangladesh</h5>
-						<p>3rd floor, CB 211/4, VIP Road, Kochukhet, Bhashantek, Dhaka 1215.</p>
-						<a href="tel:+8801888680010">+880 18 8868 0010</a>
-						<a href="mailto:info@totaltechglobal.com">info@totaltechglobal.com</a>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="contact-info-content">
-						<h5>New York Office</h5>
-						<p>15015 Sanford avenue, <br>flushing. NY11355.</p>
-						<a href="tel:+19294146358">+1 (929) 414-6358</a>
-						<a href="mailto:info@totaltechglobal.com">info@totaltechglobal.com</a>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="contact-info-content">
-						<h5>Panama Office</h5>
-						<p>568 Collins street <br>Melbourne Victoria Australia</p>
-						<a href="tel:+61406936703">+61 (406) 936703</a>
-						<a href="mailto:info@totaltechglobal.com">info@totaltechglobal.com</a>
-					</div>
-				</div>
+			<div class="single-privacy">
+				<h3 class="mt-0">Your User Account and Content</h3>
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur</p>
+				
+				<h3>Your Use of Our Service:</h3>
+				<p>1. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+				<p>2. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+				<p>3. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+				<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				
+				<h3>Reporting Copyright and Other IP Violations</h3>
+				<p><strong>a. Ownership:</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+				
+				<p><strong>b. Content Ownership:</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+				
+				<p><strong>c. Rights in User Content</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				
+				<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 			</div>
 		</div>
-    </section>
-	<!-- End Contact Box Area -->
-
-	<!-- Start Contact Section -->
-	<div class="contact-section bg-grey section-padding">
-		<div class="container">
-			<div class="section-title">
-				<h6>Contact Us</h6>
-				<h2>Let's Talk</h2>
-			</div>
-			<div class="row align-items-center">
-				<div class="col-lg-10 offset-lg-1">
-					<div class="contact-form">
-						<p class="form-message"></p><br />
-						<form id="contact-form" class="contact-form form" action="mail.php" method="POST">
-							<div class="row">
-								<div class="col-lg-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="name" id="name" class="form-control" required placeholder="Your Name">
-									</div>
-								</div>
-								<div class="col-lg-6 col-md-6">
-									<div class="form-group">
-										<input type="email" name="email" id="email" class="form-control" required placeholder="Your Email">
-									</div>
-								</div>
-								<div class="col-lg-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="phone" id="phone" required class="form-control" placeholder="Your Phone">
-									</div>
-								</div>
-								<div class="col-lg-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="subject" id="subject" class="form-control" required placeholder="Your Subject">
-									</div>
-								</div>
-								<div class="col-lg-12 col-md-12">
-									<div class="form-group">
-										<textarea name="message" class="form-control" id="message" cols="30" rows="6" required placeholder="Your Message"></textarea>
-									</div>
-								</div>
-								<div class="col-lg-12 col-md-12">
-									<button type="submit" class="default-btn submit-btn">Send Message <span></span></button>
-								</div>
-							</div>
+	</section>
+	<!-- End Inner Privacy Policy Area -->
+	
+	<!-- Start Footer & Subscribe Section -->
+	<section class="footer-subscribe-wrapper">
+		<!-- Start Subscribe Area -->
+		<div class="subscribe-area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6 col-md-6">
+						<div class="subscribe-content">
+							<h2>Sign Up Our Newsletter</h2>
+							<span class="sub-title">We Offer An Informative Monthly Technology Newsletter - Check It Out.</span>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6">
+						<form class="newsletter-form">
+							<input type="email" class="input-newsletter" placeholder="Enter your email" name="EMAIL" required autocomplete="off">
+							<button type="submit">Subscribe Now</button>
+							<div id="validator-newsletter" class="form-result"></div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- End Contact Section -->
-
-    <!-- Map Section Start -->
-    <div class="map-area">
-        <div class="map-content">
-            <div class="map-canvas" id="contact-map"></div>
-        </div>
-    </div>
-    <!-- Map Section End -->
+		<!-- End Subscribe Area -->
+	</section>
 
 @endsection
